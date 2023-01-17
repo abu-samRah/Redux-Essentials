bf61b999fe27285774bb3dc16ae77f30bb15021a
abuSamrah <abdallahabuamrah@gmail.com>
2023-01-17 16:28:25 +0200

    Added TimeAge component

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
   {
@@ -6,14 +7,14 @@ const initialState = [
     title: 'First Post!',
     content: 'Hello!',
     user: '0',
-    date: new Date().toISOString(),
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
   },
   {
     id: '2',
     title: 'Second Post',
     content: 'More text',
     user: '1',
-    date: new Date().toISOString(),
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
   },
 ]
 
